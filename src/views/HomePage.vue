e3415559f4611c04d21eabc617cd3dc7c4fa2ccc
Josh Thomas <jthoms1@gmail.com>
2023-02-03 18:44:45 -0600

    Update content on intro page.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -36,12 +36,105 @@ export default {
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <div id="container">
+      <div className="container">
+        <img className="logo" src="/assets/logo.svg" />
+        <h3>Welcome to Portals</h3>
+        <p>
+          This is a web application that has the Portals plugin preinstalled. If
+          you would like to learn more about how to interact with this web
+          application from the Native application you can find reference
+          material below.
+        </p>
+        <ol className="info-list">
+          <li>
+            <p>Passing Data to a Portal with Initial Context</p>
+            <a
+              href="https://ionic.io/docs/portals/for-ios/how-to/using-the-portals-plugin#initial-context"
+            >
+              iOS
+            </a>
+            <a
+              href="https://ionic.io/docs/portals/for-android/how-to/using-the-portals-plugin#initial-context"
+            >
+              Android
+            </a>
+          </li>
+          <li>
+            <p>Communicating with Pub/Sub</p>
+            <a
+              href="https://ionic.io/docs/portals/for-ios/how-to/using-the-portals-plugin#communicating-via-pubsub"
+            >
+              iOS
+            </a>
+            <a
+              href="https://ionic.io/docs/portals/for-android/how-to/using-the-portals-plugin#communicating-via-pubsub"
+            >
+              Android
+            </a>
+          </li>
+          <li>
+            <p>Define your own API for communication</p>
+            <a
+              href="https://ionic.io/docs/portals/for-ios/how-to/define-api-in-typescript"
+            >
+              iOS
+            </a>
+            <a
+              href="https://ionic.io/docs/portals/for-android/how-to/define-api-in-typescript"
+            >
+              Android
+            </a>
+          </li>
+        </ol>
+        <h3>Initial Context</h3>
+        <p>
+          This application was preseeded with an Initial Context. This data was
+          initially provided by the Native application
+        </p>
         <pre>{{ JSON.stringify(initialContext, null, 2) }}</pre>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 50px 25px;
+  line-height: 1.5em;
+}
+
+img.logo {
+  margin: 0 auto 38px;
+  display: block;
+  width: 33%;
+}
+
+.info-list {
+  padding: 15px;
+  list-style: none;
+}
+
+.info-list li {
+  display: flex;
+  flex-flow: row wrap;
+  margin: 8px 0;
+}
+
+.info-list p {
+  margin: 0;
+  width: 100%;
+}
+
+.info-list a {
+  display: block;
+  margin: 8px;
+}
+
+pre {
+  color: #ffffff;
+  background-color: #383a3e;
+  font-family: "Courier New", Courier, monospace;
+  margin: 0;
+  padding: 16px 8px;
+}
+</style>
